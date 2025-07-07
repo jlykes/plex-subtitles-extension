f2752650746b69d14d21f10a12ff745052fe4851
Joshua Lykes <joshua.lykes@gmail.com>
2025-07-07 01:34:26 -0700

    Some initial experiments expanding to other platforms

---
+++ platform expansion/PlexDLNABrowser/PlexDLNABrowser/PlexDLNABrowserApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlexDLNABrowserApp.swift
+//  PlexDLNABrowser
+//
+//  Created by Joshua Lykes on 7/7/25.
+//
+
+import SwiftUI
+
+@main
+struct PlexDLNABrowserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ platform expansion/PlexDLNABrowser/PlexDLNABrowserTests/PlexDLNABrowserTests.swift
@@ -0,0 +1,17 @@
+//
+//  PlexDLNABrowserTests.swift
+//  PlexDLNABrowserTests
+//
+//  Created by Joshua Lykes on 7/7/25.
+//
+
+import Testing
+@testable import PlexDLNABrowser
+
+struct PlexDLNABrowserTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
